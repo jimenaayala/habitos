a93e1068b0084112f0b5f29b564e24d7e35f27f0
Jimena Ayala <jayala@silice.biz>
2025-09-23 15:17:13 -0300

    definicion del array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-console.log("Hola");
-console.log("H");
-console.log("H");
+let habitos = [];
+habitos.push(["Ir al Gym", true, new Date()]);
+habitos.push(["Ir a Correr", true, new Date()]);
+habitos.push(["Programar", true, new Date()]);
+habitos.push(["Comer saludable", true, new Date()]);
+
+console.log(habitos[3]);
